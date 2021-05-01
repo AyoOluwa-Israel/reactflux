842a411b96c158488c82e4add32d48348a7727b5
AyoOluwa-Israel <izzyfresher@gmail.com>
2021-05-01 15:06:20 +0100

    Pages Updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 // import logo from './logo.svg';
 // import './App.css';
 
+import AboutPage from "./components/AboutPage";
+import Header from "./components/common/Header";
+import HomePage from "./components/HomePage";
+
 function App() {
-  return (
-    <div className="App">
-      
+
+  function getPage() {
+    const route = window.location.pathname;
+
+  if (route === "/about") return <AboutPage/>
+  return <HomePage/>
+  }
+  return(
+    <div className="container-fluid">
+      <Header/>
+      {getPage()}
     </div>
-  );
+  )
 }
 
 export default App;
